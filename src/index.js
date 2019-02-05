f4829fd4db06226ba095ad9411f9f1ac320dd74f
Mallarie <mallariemilano@gmail.com>
2019-02-05 15:16:00 -0800

    thank you page completed

--- src/index.js
+++ src/index.js
@@ -5,12 +5,24 @@ const positions = document.getElementById('position');
 mobForm.addEventListener('submit', function(event){
     event.preventDefault();
 
-    const chosenWeapon = [];
+    const chosenWeapons = [];
 
     for(let index = 0; index < mobForm.weapon.length; index++) {
         const weaponName = mobForm.weapon[index];
         if(weaponName.checked) {
-            chosenWeapon[index] = weaponName.value;
+            chosenWeapons[index] = weaponName.value;
         }
     }
+
+    const applicant = {
+        name: applicantName.value,
+        position: positions.value,
+        chosenWeapons: chosenWeapons
+    };
+
+    window.location = 'thanks.html';
+
+    const serialize = JSON.stringify(applicant);
+    window.localStorage.setItem('applicant', serialize);
+    
 });
